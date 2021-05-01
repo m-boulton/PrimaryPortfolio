c2ce34695965a5dc64f73a9a8e0980b3dfeeb12e
michael <boultonmichael@gmail.com>
2021-05-01 05:49:30 -0600

    saving before de-griding layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 // use dotenv
+window.addEventListener("load", () => {
+  viewHeight();
+});
+window.addEventListener("resize", () => {
+  viewHeight();
+});
+function viewHeight() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
