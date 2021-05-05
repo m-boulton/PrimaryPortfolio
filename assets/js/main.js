e0b06cf76b02259635152338f4ae41a1cfca8c4f
michael <boultonmichael@gmail.com>
2021-05-05 16:33:13 -0600

    finished sytling for about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,6 @@ function viewHeight() {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("background").style.filter = "brightness(100%)";
+});
