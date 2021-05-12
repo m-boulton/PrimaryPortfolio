a62a392a916ab612bf3d3644ac7590b07d6d5bd8
michael <boultonmichael@gmail.com>
2021-05-12 15:12:07 -0600

    finished styling of contact form mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,24 @@ function viewHeight() {
 window.addEventListener("DOMContentLoaded", () => {
   document.getElementById("background").style.filter = "brightness(100%)";
 });
+
+// // Get the forms input elements
+// let inputContainer = document.getElementById("contactForm");
+
+// // Get all inputs with class="input-box" inside the contact form
+// let inputs = inputContainer.getElementsByClassName("input-box");
+
+// // Loop through the inputs and add the active class to the current/clicked input
+// for (let i = 0; i < inputs.length; i++) {
+//   inputs[i].addEventListener("click", function () {
+//     let current = document.getElementsByClassName("input-box");
+
+//     // If there's no active class
+//     if (current.length > 0) {
+//       current[0].className = current[0].className.replace(" active", "");
+//     }
+
+//     // Add the active class to the current/clicked button
+//     this.className += " active";
+//   });
+// }
