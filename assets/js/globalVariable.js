8b8ee254e74ccb6c2a1b433d607ff21a1fff62d6
michael <boultonmichael@gmail.com>
2021-06-09 00:44:26 -0600

    finalized styling on contact form for all platforms

--- assets/js/globalVariable.js
+++ assets/js/globalVariable.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://api.mboulton.com";
-// const apiUrl = "http://localhost:3501";
+// const apiUrl = "https://api.mboulton.com";
+const apiUrl = "http://localhost:3501";
 
 export { apiUrl };
