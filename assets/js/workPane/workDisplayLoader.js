5ffd8b4c70c367a53bc0a2d179473ac45a932702
michael <boultonmichael@gmail.com>
2021-07-02 22:26:53 -0600

    implemented code display for select input

--- assets/js/workPane/workDisplayLoader.js
+++ assets/js/workPane/workDisplayLoader.js
@@ -1,12 +1,56 @@
 import getProjectData from "./getProjectData.js";
-import * as vr from "./../../styles/prettify/prettify.js";
+
+function optionBuilder([fileTypeName, array]) {
+  const optionsList = array.map((item) => {
+    return `<option value="${item.fileName}">${item.fileName}</option>`;
+  });
+  return `<optgroup label="${fileTypeName}">${optionsList}</optgroup>`;
+}
+
+function selectBuilder(currentProject) {
+  const htmlProjects = currentProject.filter(
+    (item) => item.fileType === "html"
+  );
+  const scssProjects = currentProject.filter(
+    (item) => item.fileType === "scss"
+  );
+  const jsProjects = currentProject.filter((item) => item.fileType === "js");
+  const etcProjects = currentProject.filter(
+    (item) =>
+      item.fileType !== "html" &&
+      item.fileType !== "scss" &&
+      item.fileType !== "js"
+  );
+  const split = [
+    ["HTML", htmlProjects],
+    ["SASS", scssProjects],
+    ["Javascript", jsProjects],
+    ["ETC...", etcProjects],
+  ];
+  return split.map((item) => optionBuilder(item)).join("");
+}
 
 async function workDisplayLoader(projectName) {
   const current = await getProjectData(projectName);
-  let input = document.getElementById("codeDisplay");
-  input.classList.remove("prettyprinted");
-  input.innerHTML = `<code class="language-${current[4].fileType}">${current[4].data}</code>`;
-  prettyPrint();
+  const projectSelect = await selectBuilder(current);
+  //
+  function fileSelector(event) {
+    const selected = event.target.value;
+    const codeDisplay = current.find(
+      (item) => item.fileName === event.target.value
+    );
+    document.getElementById("codeDisplay").classList.remove("prettyprinted");
+    document.getElementById("codeDisplay").innerHTML = codeDisplay.data;
+
+    // prettify injected code
+    prettyPrint();
+  }
+  document
+    .getElementById("fileSelect")
+    .removeEventListener("input", fileSelector);
+  // FIXME not removing event listener
+  document.getElementById("fileSelect").innerHTML = projectSelect;
+  document.getElementById("fileSelect").addEventListener("input", fileSelector);
 }
 
 export default workDisplayLoader;
