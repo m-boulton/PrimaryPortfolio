d7fff20d7eca0e23ec61224a57d5b058c8a00521
michael <boultonmichael@gmail.com>
2021-07-04 13:39:11 -0600

    created contactform popup confirmation window

--- assets/js/formScripts/contactFormButtons.js
+++ assets/js/formScripts/contactFormButtons.js
@@ -6,30 +6,45 @@ import formValidate from "./formValidate.js";
 async function contactFormButtons(e) {
   //   click event for submit button
   if (e.target.id == "submitButton") {
+    let cover = document.getElementById("formMessageCover");
+    let message = document.getElementById("formMessageText");
+    let button = document.getElementById("formMessageButton");
     //   build the form (object)
     let formData = await formBuilder();
     // check to make sure require fields arent empty (boolean or message)
     let formValid = await formValidate(formData);
-
     // sends form data (object) to be processed to backend and renders message in the form
     if (formValid === true) {
       // renders a message when submitting the form with fetch post
-      document.getElementById("formMessageOutput").textContent =
-        "Submitting form...";
-      let response = await formSubmit(formData);
-      if (response.message === "ERROR")
-        return (document.getElementById(
-          "formMessageOutput"
-        ).textContent = `${response.error}`);
+      message.textContent = "Submitting form...";
+      button.textContent = "Waiting";
+      cover.style.display = "block";
+
+      // Submits the form and waits for response
+      const response = await formSubmit(formData);
+      if (response.message === "ERROR") {
+        button.addEventListener("click", () => {
+          cover.style.display = "none";
+        });
+        message.textContent = `${response.error}`;
+        return (button.textContent = "Close");
+      }
       // returns out of the function and renders the server response (obj.data)
-      await formReset();
-      return (document.getElementById(
-        "formMessageOutput"
-      ).textContent = `${response.data}`);
+      button.addEventListener("click", () => {
+        formReset();
+        cover.style.display = "none";
+      });
+      message.textContent = `${response.data}`;
+      return (button.textContent = "Close");
     }
 
     // outputs error message if required fields are empty
-    document.getElementById("formMessageOutput").textContent = formValid;
+    message.textContent = formValid;
+    button.textContent = "Close";
+    cover.style.display = "block";
+    button.addEventListener("click", () => {
+      cover.style.display = "none";
+    });
   }
 
   //  click event for reset button
